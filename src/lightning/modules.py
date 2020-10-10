cc721bbce1b3bfc4d158119dcd82010470fa53a5
Tilman Krokotsch <t.krokotsch@gmail.com>
2020-10-10 17:26:20 +0200

    Add draft for lightning module.

---
+++ src/lightning/modules.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+
+
+class Autoencoder(pl.LightningModule):
+    def __init__(self, encoder, bottleneck, decoder, lr=0.01):
+        super().__init__()
+
+        self.encoder = encoder
+        self.bottleneck = bottleneck
+        self.decoder = decoder
+
+        self.lr = lr
+        self.criterion_recon = nn.MSELoss(reduction='mean')
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
+
+    def forward(self, inputs):
+        encoded = self.encoder(inputs)
+        latent_code, _ = self.bottleneck(encoded)
+        decoded = self.decoder(latent_code)
+
+        return decoded
+
+    def training_step(self, inputs, batch_idx):
+        inputs, _ = inputs
+        encoded = self.encoder(inputs)
+        latent_code, bottleneck_loss = self.bottleneck(encoded)
+        decoded = self.decoder(latent_code)
+
+        recon_loss = self.criterion_recon(inputs, decoded)
+        loss = recon_loss + bottleneck_loss
+
+        self.log('train/recon', recon_loss)
+        self.log('train/bottleneck', bottleneck_loss)
+        self.log('train/loss', loss)
+
+        return loss
+
+    # def validation_step(self, inputs):
+    #     pass
+    #
+    # def test_step(self, inputs):
+    #     pass
