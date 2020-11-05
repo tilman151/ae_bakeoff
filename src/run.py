ab5bc038fc94298d7be3d89004ef344b5bacfe76
Tilman Krokotsch <t.krokotsch@gmail.com>
2020-11-05 22:53:48 +0100

    Extend run script for different types of AE.

--- src/run.py
+++ src/run.py
@@ -1,24 +1,52 @@
 import pytorch_lightning as pl
-import torchvision
 
-import data
-import lightning.modules
-from models import encoders, decoders, bottlenecks
+from building import build_ae, build_datamodule
+
+autoencoders = ['shallow',
+                'vanilla',
+                'stacked',
+                'sparse',
+                'denoising',
+                'vae',
+                'beta_vae_strict',
+                'beta_vae_loose',
+                'vq']
 
 
-def run():
+def run(model_type):
+    assert model_type in autoencoders
     pl.seed_everything(42)
+    datamodule = build_datamodule(model_type)
+    ae = build_ae(model_type, datamodule.dims)
+    _train(model_type, ae, datamodule)
 
-    datamodule = data.MNISTDataModule('../data')
 
-    encoder = encoders.DenseEncoder(datamodule.dims, 3, 32)
-    decoder = decoders.DenseDecoder(32, 3, datamodule.dims)
-    bottleneck = bottlenecks.IdentityBottleneck()
-    ae = lightning.modules.Autoencoder(encoder, bottleneck, decoder)
+def _train(model_type, ae, datamodule):
+    epochs = 60
+    if model_type == 'stacked':
+        _train_stacked(ae, datamodule, epochs)
+    else:
+        _train_normal(ae, datamodule, epochs)
 
-    trainer = pl.Trainer(max_epochs=50, deterministic=True)
+
+def _train_stacked(ae, datamodule, epochs):
+    num_stacking = ae.encoder.num_layers
+    trainer = pl.Trainer(max_epochs=epochs // num_stacking, deterministic=True)
+    for i in range(num_stacking):
+        trainer.fit(ae, datamodule=datamodule)
+        ae.encoder.stack_layer()
+        ae.decoder.stack_layer()
+
+
+def _train_normal(ae, datamodule, epochs):
+    trainer = pl.Trainer(max_epochs=epochs, deterministic=True)
     trainer.fit(ae, datamodule=datamodule)
 
 
 if __name__ == '__main__':
-    run()
+    import argparse
+    parser = argparse.ArgumentParser(description='Run unsupervised autoencoder training.')
+    parser.add_argument('model_type', choices=autoencoders)
+    opt = parser.parse_args()
+
+    run(opt.model_type)
